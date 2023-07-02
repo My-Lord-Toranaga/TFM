8b1cc01247ee81f128d92a81e42ec6c1c7711d1a
My-Lord-Toranaga <138297340+My-Lord-Toranaga@users.noreply.github.com>
2023-07-02 19:58:08 +0530

    Final commot

--- main.tf
+++ main.tf
@@ -2,7 +2,16 @@ provider "aws" {
   region = "us-west-2"
 
 }
+data "aws_vpc" "default" {
+  default = true
+}
 
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
 variable "server_port" {
   description = "The port that the server will use for HTTP requests"
   type        = number
@@ -20,23 +29,122 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "Public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "Public IP of web server"
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 10
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "TF-asg-example"
+    propagate_at_launch = true
+  }
+
+
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-03f65b8614a860c29"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "TFM-Instance"
+resource "aws_lb_listener_rule" "Asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
   }
-  vpc_security_group_ids      = [aws_security_group.instance.id]
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+}
+
+output "alb_dns_name" {
+
+  value = aws_lb.example.dns_name
+  discription = "The domain name of Load balancer "
+}
+
+resource "aws_lb" "example" {
+  name               = "tf-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "tf-asg-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+
+
+
+  }
+}
+
+
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  security_groups   = [aws_security_group.alb.id]
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404:page not found"
+      status_code  = 404
+    }
+  }
+
+}
+
+resource "aws_security_group" "alb" {
+  name = "TF-ALB"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-03f65b8614a860c29"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
   user_data                   = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
+  lifecycle {
+    create_before_destroy = true
+  }
 
 }
