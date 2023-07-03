540fff7e786151130727e9b114d40bf61e29088d
My-Lord-Toranaga <138297340+My-Lord-Toranaga@users.noreply.github.com>
2023-07-03 20:19:24 +0530

    commit

--- C2/main.tf
+++ C2/main.tf
@@ -1,23 +1,34 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "tf-194748"
-    key= "global/s3/terraform.tfstate"
+    key    = "global/s3/terraform.tfstate"
     region = "us-west-2"
 
-    dynamodb_table = "terraform_locks"
-    encrypt=true
+    dynamodb_table = "terraform_up_and_running_locks"
+    encrypt        = true
   }
 }
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "tf-194748"
-    lifecycle {
-      prevent_destroy = true
-    }
-    
+  bucket = "tf-194748"
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of s3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of Dynamo DB table"
+
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
@@ -25,8 +36,8 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
   versioning_configuration {
     status = "Enabled"
-  }  
-  
+  }
+
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
@@ -37,25 +48,25 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
       sse_algorithm = "AES256"
     }
   }
-  
+
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.terraform_state.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
-  
+
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "terraform_up_and_running_locks"
+  name         = "terraform_up_and_running_locks"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
   attribute {
-    name ="LockID"
+    name = "LockID"
     type = "S"
   }
-  
+
 }
