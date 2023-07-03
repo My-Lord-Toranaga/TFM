1d60f20985edd6944573b6a12f0313b37411136a
My-Lord-Toranaga <138297340+My-Lord-Toranaga@users.noreply.github.com>
2023-07-03 14:26:50 +0530

    second commit

--- C2/main.tf
+++ C2/main.tf
@@ -8,3 +8,42 @@ resource "aws_s3_bucket" "terraform_state" {
     }
     
 }
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = "aws_s3_bucket.terraform_state.id"
+
+  versioning_configuration {
+    status = "enabled"
+  }  
+  
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = "aws_s3_bucket.terraform_state.id"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+  
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform_up_and_running_locks"
+  hash_key = "LockID"
+  attribute {
+    name ="LockID"
+    type = "s"
+  }
+  
+}
